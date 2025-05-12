693d9bc740aa07ce3c6ea723231728ac45e24e75
Elayas569 <ortegaaguilarelias@gmail.com>
2025-05-12 13:40:44 -0600

    prepare for deploy

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,16 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
 
+const isProduction = process.env.NODE_ENV === "production";
+
 // https://vite.dev/config/
 export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:8000",
+        target: isProduction
+          ? "https://ordinariowebback.onrender.com"
+          : "http://localhost:8000",
         secure: false,
         changeOrigin: true,
       },
